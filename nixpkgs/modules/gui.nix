f4734942996e144f218ba30776daa8c76183e2fa
Quido Meijer <quidome@gmail.com>
2021-02-15 18:02:15 +0100

    Add alacritty and manage config file

--- nixpkgs/modules/gui.nix
+++ nixpkgs/modules/gui.nix
@@ -1,8 +1,18 @@
 { config, pkgs, ... }:
 
 {
+  # setup programs
+  programs = {
+    alacritty.enable = true;
+  };
 
+  # setup services
   services = {
     flameshot.enable = true;
+  };
+
+  # manage config files
+  xdg.configFile = {
+    "alacritty/alacritty.yml".source = ../xdg-config/alacritty.yml;
   };
 }
