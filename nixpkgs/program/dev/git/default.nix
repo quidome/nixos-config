fdbcc8a8accf151296d1463716fe24ba7dd0fc13
Quido Meijer <quidome@gmail.com>
2021-03-06 16:07:02 +0100

    add git module

---
+++ nixpkgs/program/dev/git/default.nix
@@ -0,0 +1,56 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let
+  cfg = config.elemental.program.dev.git;
+in
+{
+  options.elemental.program.dev.git = {
+    enable = lib.mkEnableOption "Enable git";
+
+    userEmail = lib.mkOption {
+      type = types.lines;
+      description = "The git config email";
+      default = "";
+    };
+
+    userName = lib.mkOption {
+      type = types.lines;
+      description = "The git config name";
+      default = "";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.git = {
+      enable = true;
+      userEmail = cfg.userEmail;
+      userName = cfg.userName;
+      delta.enable = true;
+      extraConfig = {
+        core.excludesfile = "${config.home.homeDirectory}/.gitignore_global";
+        # Force SSH
+#        url = {
+#          "git@gitlab.bol.io:" = {
+#            insteadOf = "https://gitlab.bol.io/";
+#          };
+#        };
+        branch.autosetuprebase = "always";
+        color.ui = "auto";
+        diff.tool = "meld";
+        difftool.prompt = false;
+        difftool = {
+          "meld" = {
+            cmd = "meld \"$LOCAL\" \"$REMOTE\"";
+          };
+        };
+        pull.rebase = true;
+        rebase.autoStash = true;
+      };
+      # Aliases
+      aliases = {
+        "dt" = "difftool --tool=meld --dir-diff";
+        "praise" = "blame";
+      };
+    };
+  };
+}
