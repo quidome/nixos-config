be5eede77dd23b39d356751014032834af09b06b
Quido Meijer <quidome@gmail.com>
2021-03-06 18:23:16 +0100

    Store local changes

--- nixpkgs/hosts/coolding/home.nix
+++ nixpkgs/hosts/coolding/home.nix
@@ -11,15 +11,23 @@ in {
     ../../modules/gui.nix
     ../../modules/i3.nix
     ../../program
+    ../../role
+    ../../role/de/gnome
     ./secret.nix
   ];
-
-  # feature toggles
-  elemental.home.program.terminal.tmux.enable = true; 
 
   home.username = user;
   home.homeDirectory = userHome;
   home.stateVersion = "20.09";
+
+  # elemental setup
+  elemental.user = user;
+  elemental.role = "workstation";
+  elemental.machine = hostname;
+
+  # feature toggles
+  elemental.program.terminal.tmux.enable = true;
+  elemental.role.de.gnome.enable = true;
 
   nixpkgs.config.allowUnfree = true;
   home.packages = with pkgs; [
