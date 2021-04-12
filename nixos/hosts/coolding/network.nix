d5cce07909da0f68a0e08430714abfd24fc75b5c
Quido Meijer <quidome@gmail.com>
2021-04-12 18:22:49 +0200

    Disable networkmanager when using wpa_supplicant config

--- nixos/hosts/coolding/network.nix
+++ nixos/hosts/coolding/network.nix
@@ -7,6 +7,8 @@
       networking.interfaces.enp1s0f0.useDHCP = true;
       networking.interfaces.wlp2s0.useDHCP = true;
       networking.interfaces.enp0s20f0u1u2u3.useDHCP = true;
+
+      networking.networkmanager.enable = false;
     })
 
     ( lib.mkIf (! config.custom.wpa.enable) {
