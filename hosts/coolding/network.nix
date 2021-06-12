935179dfe3c7eb6e35ba2c0bca7da86cde39ba4d
Quido Meijer <quidome@gmail.com>
2021-06-12 20:10:57 +0200

    add wpa interface for startup

--- hosts/coolding/network.nix
+++ hosts/coolding/network.nix
@@ -4,9 +4,10 @@
     ( lib.mkIf config.custom.wpa.enable {
       # configuration for wpa supplicant
       networking.wireless.enable = true;
+      networking.wireless.interfaces = [ "wlp2s0" ] ;
+
       networking.interfaces.enp1s0f0.useDHCP = true;
       networking.interfaces.wlp2s0.useDHCP = true;
-
       networking.networkmanager.enable = false;
     })
 
