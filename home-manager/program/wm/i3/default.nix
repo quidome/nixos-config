bcebf81fd825ffa7cd3dad6996e1e0ae7aefefed
Quido Meijer <quidome@gmail.com>
2021-06-12 16:44:19 +0200

    Move i3status-rust config to home manager

--- home-manager/program/wm/i3/default.nix
+++ home-manager/program/wm/i3/default.nix
@@ -16,6 +16,65 @@ in
       arc-icon-theme
     ];
 
+
+    programs.i3status-rust = {
+      enable = true;
+      bars.top = {
+        icons = "awesome";
+        theme = "slick";
+        blocks = [
+          {
+            block="load";
+            interval = 5;
+            format = "{5m}";
+          }
+          {
+            block = "memory";
+            format_mem = "{mem_used_percents}";
+            warning_mem = 90.0;
+            critical_mem = 95.0;
+            interval = 5;
+          }
+          {
+            block = "disk_space";
+            warning = 10.0;
+            alert = 5.0;
+            format = "/ {available}";
+          }
+          {
+            block = "docker";
+            interval = 2;
+            format = "{running}/{total}";
+          }
+          {
+            block = "net";
+            device = "wlp2s0";
+            interval = 5;
+            format = "{ssid} ({signal_strength})";
+          }
+          {
+            block = "net";
+            device = "enp0s31f6";
+            format = "{bitrate}";
+          }
+          { block = "sound"; }
+          {
+            block = "battery";
+            format = "{percentage} {time}";
+          }
+          {
+            block = "time";
+            interval = 60;
+            timezone = "Europe/Amsterdam";
+            format = "%a %d/%m %R W%W";
+          }
+          {
+            block = "taskwarrior";
+          }
+        ];
+      };
+    };
+
     services.flameshot.enable = true;
 
     xdg.configFile."mimeapps.list".force = true;
