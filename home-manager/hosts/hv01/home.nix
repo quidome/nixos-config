5caac8f9d1d5c783d23f7f13a2ae00748d925a95
Quido Meijer <quidome@gmail.com>
2021-07-15 22:20:12 +0200

    Add home manager config for hv01

---
+++ home-manager/hosts/hv01/home.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+let
+  user = "quidome";
+  userHome = "/home/${user}";
+  hostname = "hv01";
+in
+{
+  imports = [
+    ./secret.nix
+    ../../modules
+  ];
+
+  # home manager required settings
+  home.username = user;
+  home.homeDirectory = userHome;
+  home.stateVersion = "21.05";
+
+  my = {
+    user = user;
+    role = "server";
+    host = hostname;
+
+    programs.tmux.enable = true;
+    programs.zsh.enable = true;
+  };
+}
